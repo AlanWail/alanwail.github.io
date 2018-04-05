a0fb5353c7b81c5b93b8f78eb933df93b76bf45e
AlanWail <37931261+AlanWail@users.noreply.github.com>
2018-04-05 08:56:00 +0000

    Update foot.js

--- m/js/foot.js
+++ m/js/foot.js
@@ -1,4 +1,4 @@
 document.writeln("<footer class=\'footer-site\'>");
-document.writeln("<p class=\'links\' style=\'font-size: 14px\' ><a href=\'https://www.duba.com/?un_454974_121402\'>µçÄÔ°æ</a></p>");
-document.writeln("<p class=\'ex\' style=\'font-size: 14px\'>Copyright ÍøÖ·µ¼º½ All Rights Reserved</p>");
+document.writeln("<p class=\'links\' style=\'font-size: 14px\' ><a href="http://www.hermitlan.com">電腦版</a></p>");
+document.writeln("<p class=\'ex\' style=\'font-size: 14px\'>Copyright Hermit'Lantern All Rights Reserved</p>");
 document.writeln("</footer>"); 

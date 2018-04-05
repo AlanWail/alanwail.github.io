adfd84290f85d0649833fab3ccfb00f459ec909a
AlanWail <37931261+AlanWail@users.noreply.github.com>
2018-04-05 08:54:03 +0000

    Update foot.js

--- m/js/foot.js
+++ m/js/foot.js
@@ -1,4 +1,4 @@
 document.writeln("<footer class=\'footer-site\'>");
-document.writeln("<p class=\'links\' style=\'font-size: 14px\' ><a href=\'https://www.duba.com/?un_454974_121402\'>µçÄÔ°æ</a></p>");
-document.writeln("<p class=\'ex\' style=\'font-size: 14px\'>Copyright ÍøÖ·µ¼º½ All Rights Reserved</p>");
-document.writeln("</footer>");
+document.writeln("<p class=\'links\' style=\'font-size: 14px\' ><a href=\'https://www.duba.com/?un_454974_121402\'>ÂµÃ§Ã„Ã”Â°Ã¦</a></p>");
+document.writeln("<p class=\'ex\' style=\'font-size: 14px\'>Copyright ÃÃ¸Ã–Â·ÂµÂ¼ÂºÂ½ All Rights Reserved</p>");
+document.writeln("</footer>"); 

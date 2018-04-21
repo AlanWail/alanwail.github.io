686985c1544d2106f26af8721c41d6bf4e670dcc
AlanWail <37931261+AlanWail@users.noreply.github.com>
2018-04-21 13:21:51 +0800

    Update footdaohang.js

--- m/navigate/js/footdaohang.js
+++ m/navigate/js/footdaohang.js
@@ -1,4 +1,4 @@
 document.writeln("<footer class=\'footer-site\'>");
-document.writeln("<p class=\'links\' style=\'font-size: 14px\' ><a href=\'https://www.hermitlan.com/'>電腦版</a></p>");
-document.writeln("<p class=\'ex\' style=\'font-size: 14px\'>Copyright© Hermit's Lantern. All Rights Reserved.</p>");
+//document.writeln("<p class=\'links\' style=\'font-size: 14px\' ><a href=\'https://www.hermitlan.com/'>電腦版</a></p>");
+document.writeln("<p class=\'ex\' style=\'font-size: 14px\'>Copyright© 2018-2020 Hermit's Lantern. All Rights Reserved.</p>");
 document.writeln("</footer>");

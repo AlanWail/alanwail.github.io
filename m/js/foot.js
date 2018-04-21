d364079e07e98df381726640300666d38a289189
AlanWail <37931261+AlanWail@users.noreply.github.com>
2018-04-21 13:18:21 +0800

    Update foot.js

--- m/js/foot.js
+++ m/js/foot.js
@@ -1,4 +1,4 @@
 document.writeln("<footer class=\'footer-site\'>");
-document.writeln("<p class=\'links\' style=\'font-size: 14px\' ><a href=\'http://www.hermitlan.com\index.html'>電腦版</a></p>");
-document.writeln("<p class=\'ex\' style=\'font-size: 14px\'>Copyright © Hermit'Lantern. All Rights Reserved.</p>");
+//document.writeln("<p class=\'links\' style=\'font-size: 14px\' ><a href=\'http://www.hermitlan.com\index.html'>電腦版</a></p>");
+document.writeln("<p class=\'ex\' style=\'font-size: 14px\'>Copyright©2018-2020 Hermit's Lantern. All Rights Reserved.</p>");
 document.writeln("</footer>"); 

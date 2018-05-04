45751365c99f2bbcf36d54405d316a88b68241da
AlanWail <37931261+AlanWail@users.noreply.github.com>
2018-05-04 16:00:55 +0800

    Update search.js

--- m/navigate/js/search.js
+++ m/navigate/js/search.js
@@ -1,5 +1,5 @@
  function fas(){
 	var value = document.getElementById("input1").value;
-	window.open('https://google.cutinlove.com/search?q='+encodeURIComponent(value));
+	window.open('https://g.zhypro.me/search?q='+encodeURIComponent(value));
     //window.open('https://cn.bing.com/search?q='+encodeURIComponent(M("#J_search_input").val()));
  }
